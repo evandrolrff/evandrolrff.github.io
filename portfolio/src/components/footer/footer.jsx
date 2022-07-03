08a62ea9f5433ca3d04604a407a674c2f5eba876
Evandro <evandrolrff@gmail.com>
2022-07-03 20:21:19 -0300

    modified footer section

--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -1,11 +1,6 @@
 import "./footer.css";
 import React from 'react';
 import { BsMouse } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
-import { TiSocialDribbble } from "react-icons/ti";
-import { TiSocialYoutube } from "react-icons/ti";
-import { TiSocialGithub } from "react-icons/ti";
 
 function Footer() {
   return (
@@ -18,27 +13,6 @@ function Footer() {
           </h2>
         </a>
       </h1>
-      <div className="social-links">
-        <a href="#">
-          <BsInstagram className="social" />
-        </a>
-        <a href="#">
-          {" "}
-          <FaFacebookF className="social" />
-        </a>
-        <a href="#">
-          {" "}
-          <TiSocialDribbble className="social" />
-        </a>
-
-        <a href="#">
-          <TiSocialYoutube className="social" />
-        </a>
-
-        <a href="#">
-          <TiSocialGithub className="social" />
-        </a>
-      </div>
     </div>
   );
 }
