9c6ff9f6589b0ccc2138cf43a16a7bb1af6df619
Evandro <evandrolrff@gmail.com>
2022-07-03 20:19:28 -0300

    modified contact section

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,28 +1,25 @@
 import './contact.css';
 import React from 'react';
-import { AiOutlineYoutube } from 'react-icons/ai';
-import { AiOutlineWhatsApp } from 'react-icons/ai';
-import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
+import { MdOutlineSchool } from 'react-icons/md';
 
 function Contact(){
     return (
         <div id="contact" className='container contact-container'>
-            <h1>contact me</h1>
+            <h1>Contact me</h1>
             <div className='contact-links'>
                 
-                <a href='' className='contact youtube'>
-                    <AiOutlineYoutube className='icon' />
-                    <h2>youtube <span>-----------</span></h2>
+                <a href='' className='contact linkedin'>
+                    <AiOutlineLinkedin className='icon' />
                 </a>
 
-                <a href='' className='contact whatsapp'>
-                    <AiOutlineWhatsApp className='icon' />
-                    <h2>whatsapp <span>+00 00 0000</span></h2>
+                <a href='' className='contact lattes'>
+                    <MdOutlineSchool className='icon' />
                 </a>
 
-                <a href='' className='contact instagram'>
-                    <AiOutlineInstagram className='icon' />
-                    <h2>instagram <span>-----------</span></h2>
+                <a href='' className='contact github'>
+                    <AiFillGithub className='icon' />
                 </a>
 
             </div>
