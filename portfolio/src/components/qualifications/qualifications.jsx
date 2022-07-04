5a8e5fd2e1386713aa5999a715c88325b2ef3978
Evandro <evandrolrff@gmail.com>
2022-07-03 21:08:32 -0300

    updated qualifications

--- portfolio/src/components/qualifications/qualifications.jsx
+++ portfolio/src/components/qualifications/qualifications.jsx
@@ -4,11 +4,18 @@ import React from 'react';
 const Qualification = () => {
     return (
         <div>
-            <span className='quali-text'>My Qualifications</span>
+            <span className='quali-text'>Academic formation</span>
             <div className='qualifications-container'>
                 <div className='qualification'>
-                    <div className='content html'>
-                        <h1>Language</h1>
+                    <div className='content'>
+                        <h1>MASTER IN COMPUTER SCIENCE</h1>
+                        <span>Federal University of Santa Maria - UFSM</span>
+                        
+                        <h1>BACHELOR IN INFORMATION SYSTEMS</h1>
+                        <span>Federal University of Santa Maria - UFSM</span>
+                        
+                        <h1>COMPUTER TECHNICIAN</h1>
+                        <span>Federal University of Santa Maria - UFSM</span>
                     </div>
                 </div>
             </div>
